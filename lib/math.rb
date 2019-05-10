b4f9ff44fff772dd4d979033884cc2beb19940c7
Sam Markham <sam.e.markham@gmail.com>
2019-05-10 21:48:28 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -23,7 +23,7 @@ def square_root(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  num1 + ((num2 ** num3) / num4)
+  num1 + ((num2 * num3) / num4)
   #Hint:  __ + (( __ * __ ) / __ )
   #(7, 43, 23, 83)).to eq(18)
 end
